7a384619987e9b55c09cbb19dd08f2e7b95eef4e
Petrik <petrik@deheus.net>
2008-12-10 22:53:34 +0100

    fixed tests for rails 2.2

--- test/table_builder_test.rb
+++ test/table_builder_test.rb
@@ -4,34 +4,33 @@ class TableBuilderTest < Test::Unit::TestCase
   include ActionView::Helpers::TextHelper
   include ActionView::Helpers::TagHelper
   include TableHelper
+  attr_accessor :output_buffer  
   
   def setup
     @drummer1 = Drummer.new(1, 'John "Stumpy" Pepys')
     @drummer2 = Drummer.new(2, 'Eric "Stumpy Joe" Childs')
     @drummer3 = Drummer.new(3, 'Peter "James" Bond')
     @drummer4 = Drummer.new(4, 'Mick Shrimpton (R. J. "Ric" Parnell)')
+    self.output_buffer = ''    
   end
   
   def test_table_for
-    _erbout = ''
     table_for([], :html => { :id => 'id', :style => 'style', :class => 'class'}) do |t|
     end
     expected = %(<table id="id" style="style" class="class">) <<
       %(</table>)
-    assert_dom_equal expected, _erbout
+    assert_dom_equal expected, output_buffer
   end
 
   def test_table_for_without_an_array_raises_error
-    _erbout = ''
     assert_raises(ArgumentError) do
       table_for('a') {|t| }
     end
   end
 
   def test_table_for_head
-    _erbout = ''
     table_for([@drummer1, @drummer2]) do |t|
-      _erbout.concat t.head('Id', 'Name')
+      output_buffer.concat t.head('Id', 'Name')
     end
     expected = %(<table>) <<
         %(<thead>) <<
@@ -41,16 +40,15 @@ class TableBuilderTest < Test::Unit::TestCase
           %(</tr>) <<
         %(</thead>) <<
       %(</table>)
-    assert_dom_equal expected, _erbout
+    assert_dom_equal expected, output_buffer
   end
 
   def test_table_for_body
-    _erbout = ''
     table_for([@drummer3, @drummer4]) do |t|
       t.body do |e|
         t.r do
-          _erbout.concat t.d(e.id)
-          _erbout.concat t.d(e.name)
+          output_buffer.concat t.d(e.id)
+          output_buffer.concat t.d(e.name)
         end
       end
     end
@@ -66,15 +64,14 @@ class TableBuilderTest < Test::Unit::TestCase
           %(</tr>) <<
         %(</tbody>) <<
       %(</table>)
-    assert_dom_equal expected, _erbout
+    assert_dom_equal expected, output_buffer
   end
 
   def test_table_for_body_r
-    _erbout = ''
     table_for([@drummer3, @drummer4]) do |t|
       t.body_r do |e|
-        _erbout.concat t.d(e.id)
-        _erbout.concat t.d(e.name)
+        output_buffer.concat t.d(e.id)
+        output_buffer.concat t.d(e.name)
       end
     end
     expected = %(<table>) <<
@@ -89,14 +86,13 @@ class TableBuilderTest < Test::Unit::TestCase
           %(</tr>) <<
         %(</tbody>) <<
       %(</table>)
-    assert_dom_equal expected, _erbout
+    assert_dom_equal expected, output_buffer
   end
   
   def test_table_for_td_with_options
-     _erbout = ''
      table_for([@drummer1]) do |t|
        t.body_r do |e|
-         _erbout.concat t.d(e.name, :class => 'class')
+         output_buffer.concat t.d(e.name, :class => 'class')
        end
      end
      expected = %(<table>) <<
@@ -106,7 +102,7 @@ class TableBuilderTest < Test::Unit::TestCase
            %(</tr>) <<
          %(</tbody>) <<
        %(</table>)
-     assert_dom_equal expected, _erbout
+     assert_dom_equal expected, output_buffer
    end  
 
 end
