ebd2ece8e6677f2a622c4700d50976df3917c224
Petrik <petrik@deheus.net>
2009-06-01 08:26:16 +0200

    Added id_pattern to calendar day

--- lib/table_builder/calendar_helper.rb
+++ lib/table_builder/calendar_helper.rb
@@ -22,12 +22,13 @@ module CalendarHelper
       raise ArgumentError, "Missing block" unless block_given?
       options = options_from_hash(args)
       day_method = options.delete(:day_method) || :date
+      id_pattern = options.delete(:id)
       tbody do
         @calendar.objects_for_days(@objects, day_method).to_a.sort{|a1, a2| a1.first <=> a2.first }.each do |o|
           key, array = o
           day, objects = array
           concat(tag(:tr, options, true)) if(day.wday ==  @calendar.first_weekday)
-          concat(tag(:td, td_options(day), true))
+          concat(tag(:td, td_options(day, id_pattern), true))
           yield(day, objects)
           concat('</td>')
           concat('</tr>') if(day.wday ==  @calendar.last_weekday)
@@ -41,7 +42,7 @@ module CalendarHelper
       @calendar.objects_for_days(@objects)
     end
     
-    def td_options(day)
+    def td_options(day, id_pattern)
       options = {}
       if(day.strftime("%Y-%m-%d") ==  @today.strftime("%Y-%m-%d"))
         options[:class] = 'today'
@@ -49,6 +50,9 @@ module CalendarHelper
         options[:class] = 'notmonth'
       elsif(day.wday == 0 or day.wday == 6)
         options[:class] = 'weekend'
+      end
+      if id_pattern
+        options[:id] = day.strftime(id_pattern)
       end
       options
     end
--- test/calendar_helper_test.rb
+++ test/calendar_helper_test.rb
@@ -85,6 +85,26 @@ class CalendarHelperTest < Test::Unit::TestCase
       %(</table>)
     assert_dom_equal expected, self.output_buffer
   end  
+  
+  def test_calendar_for_sets_css_ids
+    self.output_buffer = ''
+    calendar_for([], :year=> 2008, :month => 12, :today => Date.civil(2008, 12, 15)) do |c|
+      c.day(:id => 'day_%d') do |day, events|
+        output_buffer.concat(events.collect{|e| e.id}.join)
+      end
+    end
+    expected = %(<table>) <<
+      %(<tbody>) <<
+        %(<tr><td class="notmonth" id="day_30"></td><td id="day_01"></td><td id="day_02"></td><td id="day_03"></td><td id="day_04"></td><td id="day_05"></td><td class="weekend" id="day_06"></td></tr>) <<
+        %(<tr><td class="weekend" id="day_07"></td><td id="day_08"></td><td id="day_09"></td><td id="day_10"></td><td id="day_11"></td><td id="day_12"></td><td class="weekend" id="day_13"></td></tr>) <<
+        %(<tr><td class="weekend" id="day_14"></td><td class="today"id="day_15"></td><td id="day_16"></td><td id="day_17"></td><td id="day_18"></td><td id="day_19"></td><td class="weekend" id="day_20"></td></tr>) <<
+        %(<tr><td class="weekend" id="day_21"></td><td id="day_22"></td><td id="day_23"></td><td id="day_24"></td><td id="day_25"></td><td id="day_26"></td><td class="weekend" id="day_27"></td></tr>) <<
+        %(<tr><td class="weekend" id="day_28"></td><td id="day_29"></td><td id="day_30"></td><td id="day_31"></td><td class="notmonth" id="day_01"></td><td class="notmonth" id="day_02"></td><td class="notmonth" id="day_03"></td></tr>) <<
+        %(</tbody>) <<
+      %(</table>)
+    assert_dom_equal expected, self.output_buffer
+  end  
+  
 end
 
 class CalendarHelperTest < Test::Unit::TestCase
