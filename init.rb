b81f4db4569c859b000a9d10c5c284e87984def9
Petrik <petrik@deheus.net>
2012-02-18 13:46:46 +0100

    No longer maintained

--- init.rb
+++
@@ -1,2 +0,0 @@
-require 'table_builder'
- 
--- lib/table_builder.rb
+++
@@ -1,5 +0,0 @@
-require(File.expand_path(File.join(File.dirname(__FILE__), 'table_builder', 'table_builder.rb')))
-require(File.expand_path(File.join(File.dirname(__FILE__), 'table_builder', 'calendar_helper.rb')))
-
-ActionView::Base.send :include, TableHelper
-ActionView::Base.send :include, CalendarHelper
